6e8bb3bd49a857cd60a477256c6f767e424b7739
jpsxlr8 <54054165+jpsxlr8@users.noreply.github.com>
2022-12-08 11:36:19 +0530

    Update OriginalArrayofPrefixXor.cpp

--- Leetcode/OriginalArrayofPrefixXor.cpp
+++ Leetcode/OriginalArrayofPrefixXor.cpp
@@ -1,9 +1,9 @@
-```You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+<!--You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
 
 pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
 Note that ^ denotes the bitwise-xor operation.
 
-It can be proven that the answer is unique.```
+It can be proven that the answer is unique.-->
 
 class Solution {
 public:
