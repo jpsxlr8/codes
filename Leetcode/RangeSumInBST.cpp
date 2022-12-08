88db61efe410c12879ff0f3ee5ab8a5bb845edf5
jpsxlr8 <54054165+jpsxlr8@users.noreply.github.com>
2022-12-08 11:30:20 +0530

    added

---
+++ Leetcode/RangeSumInBST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    int inorder(TreeNode* root,int low,int high){
+        if(!root) return 0;
+        inorder(root->left,low,high);
+        if(root->val>=low && root->val<=high)
+        sum+=root->val;
+        inorder(root->right,low,high);
+        return sum;
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+        return inorder(root,low,high);
+    }
+};
