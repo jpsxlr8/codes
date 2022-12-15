4fd0498016f63125b729376164ad55859f80eb59
jpsxlr8 <54054165+jpsxlr8@users.noreply.github.com>
2022-12-15 18:00:08 +0530

    added

---
+++ Arrays & String/PermutationInString.cpp
@@ -0,0 +1,32 @@
+// Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+// In other words, return true if one of s1's permutations is the substring of s2
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+        vector<int>count1(26,0), count2(26,0);
+        
+        int i;
+        for(i = 0; i < s1.size(); i++) {
+            // compute the frequency of each char for the first window
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
+        }
+        
+        if(count1 == count2) return true;
+        
+        // search the remaining windows
+        int start = 0, end = i;
+        while(end < s2.size()){
+        //remove char at 'start' index and add char at 'end' index
+            --count2[s2[start] - 'a'];
+            ++count2[s2[end] - 'a'];
+          
+            if(count1 == count2) return true;
+            end++; start++;
+        }
+        return false;
+    }
+};
